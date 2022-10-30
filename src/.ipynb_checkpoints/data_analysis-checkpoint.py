64eaf639e9529def63229fcfbd3fe147471672d5
AlexanderFurman <alexfurman16@.com>
2022-10-30 10:18:04 +0200

    python_tutorial

---
+++ src/.ipynb_checkpoints/data_analysis-checkpoint.py
@@ -0,0 +1,7 @@
+import numpy as np # math functions
+import scipy # scientific functions
+import matplotlib.pyplot as plt # for plotting figures and setting their properties
+import pandas as pd # handling data structures (loaded from files)
+from scipy.stats import linregress # contains linregress (for linear regression)
+from scipy.optimize import curve_fit as cfit # non-linear curve fitting
+from sklearn.metrics import r2_score # import function that calculates R^2 score 
